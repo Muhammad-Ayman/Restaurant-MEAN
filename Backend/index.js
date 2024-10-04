91111fa11f24234401d2fc3009da1be1734814d8
Muhammad-Ayman <i.muhammadayman@gmail.com>
2024-10-04 11:46:48 +0300

    last Working Shit

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("node:path");
+const dotenv = require("dotenv");
+const app = express();
+const productsRoutes = require("./routes/productsRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+
+dotenv.config();
+app.use(
+  cors({
+    origin: "http://localhost:4200", // Change this to your Angular app's URL
+    credentials: true, // Enable set cookie
+  })
+);
+app.listen(process.env.PORT, () => {
+  console.log("Server Started");
+});
+
+mongoose.connect(process.env.DB).then(() => {
+  console.log("DB Connected");
+});
+
+app.use(express.static(path.join(__dirname, "")));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/users", usersRoutes);
+app.use("/products", productsRoutes);
--- Backend/server.js
+++
@@ -1,9 +0,0 @@
-import http from "http";
-
-const app = http.createServer((req, res) => {
-  console.log(req.url);
-});
-
-app.listen(4000, () => {
-  console.log("listeing");
-});
